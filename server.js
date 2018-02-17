e6273cb1f00a5fded51d94e8b26f6ebdf5651486
Madjer <madjerbo@gmail.com>
2018-02-17 14:40:51 -0300

    Adcanced Templating

--- server.js
+++ server.js
@@ -2,22 +2,30 @@ const express = require('express');
 const hbs = require('hbs');
 
 var app = express();
-app.set('view engine', 'hbs');
 
+hbs.registerPartials(__dirname+'/views/partials');
+app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', ()=> {
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+});
+
 
 app.get('/', (req, res) => {
     res.render('home.hbs', {
-        pageTitle: 'About Page',
-        wellcomeMessage: 'Wellcome!',
-        currentYear: new Date().getFullYear()
+        pageTitle: 'Home Page',
+        wellcomeMessage: 'Wellcome!'
     });
 });
 
 app.get('/about', (req, res) => {
     res.render('about.hbs', {
-        pageTitle: 'About Page',
-        currentYear: new Date().getFullYear()
+        pageTitle: 'About Page'
     });
 });
 
