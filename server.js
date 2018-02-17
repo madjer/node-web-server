97d06b61a3646448b8abf82049c7d0c4498af35e
Madjer <madjerbo@gmail.com>
2018-02-17 14:49:46 -0300

    Express Middleware

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('rs');
 
 var app = express();
 
 hbs.registerPartials(__dirname+'/views/partials');
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
+
+app.use((req, res, next)=> {
+    let now = new Date().toString();
+    console.log(`${now}: ${req.method} ${req.url}`);
+    next(); 
+});
 
 hbs.registerHelper('getCurrentYear', ()=> {
     return new Date().getFullYear();
