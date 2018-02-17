a5277b78fcb362d64c7852c446d21f0758363811
Madjer <madjerbo@gmail.com>
2018-02-17 17:55:59 -0300

    Preparing for heroku

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
+const port = process.env.PORT || 3000;
 
 var app = express();
+
+
 
 hbs.registerPartials(__dirname+'/views/partials');
 app.set('view engine', 'hbs');
@@ -49,6 +52,7 @@ app.get('/about', (req, res) => {
 app.get('/bad', (req, res)=>{
     res.send({errorMessage: 'Unable to find the page!'});
 });
-app.listen(3000, ()=>{
-    console.log('Server is up on port 3000');
+
+app.listen(port, ()=>{
+    console.log(`Server is up on port ${port}`);
 });
